854e6cc76b8275fc5193c9fa57f949c947d2d93c
Vini33 <vb850421@gmail.com>
2022-06-13 19:00:48 -0300

    novo

--- chamajs/fotoconfig.js
+++ chamajs/fotoconfig.js
@@ -1,4 +1,4 @@
 const dados = [['O amigo de paz.jpg','O amigo de paz','8A'],
 ['O jabuti e o coelho.jpg','O jabuti e o coelho','9A'],
-['O segredo da plebeia.png','O segredo da plebeia','9A']]
+['O segredo da plebeia.jpg','O segredo da plebeia','9A']]
 export {dados}
